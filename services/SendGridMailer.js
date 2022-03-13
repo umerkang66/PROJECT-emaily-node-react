909c61565cc56610c6156f673cb0bf707b0094d4
Umer Kang <umerkang66@gmail.com>
2022-03-13 21:54:58 +0500

    add normal sendinblue sending emails test file in services directory

--- services/SendGridMailer.js
+++ services/SendGridMailer.js
@@ -1,6 +1,9 @@
 const sgMail = require('@sendgrid/mail');
+const { htmlToText } = require('html-to-text');
+const nodemailer = require('nodemailer');
+const keys = require('../config/keys');
 
-class Mailer {
+class MailerSendGrid {
   verifiedSenderEmail = 'umerkang77@gmail.com';
 
   constructor({ subject, recipients }, content) {
@@ -29,5 +32,47 @@ class Mailer {
   async send() {
     const response = await this.sgMail.send(this.msg);
     return response;
+  }
+}
+
+class MailerSendInBlue {
+  verifiedSenderEmail = 'umerkang77@gmail.com';
+
+  // Content should be provided as html template
+  constructor({ subject, recipients }, content) {
+    this.to = this.formatRecipients(recipients);
+    this.from = this.verifiedSenderEmail;
+    this.subject = subject;
+    this.text = htmlToText(content);
+    this.html = content;
+  }
+
+  formatRecipients(recipients) {
+    return recipients.map(recipient => recipient.email);
+  }
+
+  _newTransport() {
+    return nodemailer.createTransport({
+      // Using sendInBlue (that is also predefined)
+      service: 'SendinBlue',
+      auth: {
+        user: keys.sendInBlueUser,
+        pass: keys.sendInBluePass,
+      },
+    });
+  }
+
+  async send() {
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject: this.subject,
+      html: this.html,
+      text: this.text,
+    };
+
+    // 3) Create a transport and send email
+    const transporter = this._newTransport();
+    await transporter.sendMail(mailOptions);
   }
 }
